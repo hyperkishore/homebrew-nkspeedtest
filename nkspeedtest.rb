a0ec0e62bb31369aaa68ac6e2bba53323647b7cc
Kishore Natarajan <kishore@Kishores-Macbook.local>
2026-01-12 23:09:13 +0530

    Rename formula to nkspeedtest

--- nkspeedtest.rb
+++ nkspeedtest.rb
@@ -1,4 +1,4 @@
-class SpeedMonitor < Formula
+class Nkspeedtest < Formula
   desc "Internet speed monitoring with central dashboard aggregation"
   homepage "https://github.com/hyperkishore/speed-monitor"
   url "https://github.com/hyperkishore/speed-monitor/archive/refs/tags/v1.0.0.tar.gz"
@@ -8,31 +8,31 @@ class SpeedMonitor < Formula
   depends_on "speedtest-cli"
 
   def install
-    bin.install "bin/speed-monitor"
+    bin.install "bin/speed-monitor" => "nkspeedtest"
   end
 
   def post_install
-    ohai "Speed Monitor installed!"
-    ohai "Run 'speed-monitor setup' to configure"
+    ohai "nkspeedtest installed!"
+    ohai "Run 'nkspeedtest setup' to configure"
   end
 
   def caveats
     <<~EOS
       To get started:
-        speed-monitor setup
+        nkspeedtest setup
 
       To start automatic monitoring:
-        speed-monitor start
+        nkspeedtest start
 
       To view status:
-        speed-monitor status
+        nkspeedtest status
 
       To open the local dashboard:
-        speed-monitor dashboard
+        nkspeedtest dashboard
     EOS
   end
 
   test do
-    assert_match "speed-monitor", shell_output("#{bin}/speed-monitor --version")
+    assert_match "speed-monitor", shell_output("#{bin}/nkspeedtest --version")
   end
 end
